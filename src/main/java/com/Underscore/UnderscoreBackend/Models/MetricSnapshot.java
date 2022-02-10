c835c605bdbe937a769c2bee2df3a1975183db23
zainlateef <zlateefwps@gmail.com>
2022-02-10 15:48:58 -0500

    testing heroku database

--- src/main/java/com/Underscore/UnderscoreBackend/Models/MetricSnapshot.java
+++ src/main/java/com/Underscore/UnderscoreBackend/Models/MetricSnapshot.java
@@ -9,7 +9,7 @@ import java.util.Date;
 @Data
 public class MetricSnapshot {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String userId;
